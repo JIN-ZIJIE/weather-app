23ebbd762e4a4257b26db669fdf0302060f10da4
JJ Jin <106864282+JIN-ZIJIE@users.noreply.github.com>
2023-04-27 10:30:00 +0800

    update

--- js/script.js
+++ js/script.js
@@ -1,20 +1,27 @@
 const api_key = "b72777b10a060f04cecd8ca6a9992f8b";
 const url = `https://api.openweathermap.org/data/2.5/weather?q=Singapore&units=metric&appid=${api_key}`;
 
+let teamperature;
+let feels_like;
+let humidity;
+let wind_speed;
+let description;
+
 fetch(url)
   .then(response => response.json())
   .then(data => {
-    const temperature = data.main.temp;
-    const feels_like = data.main.feels_like;
-    const humidity = data.main.humidity;
-    const wind_speed = data.wind.speed;
-    const description = data.weather[0].description;
+    temperature = data.main.temp;
+    feels_like = data.main.feels_like;
+    humidity = data.main.humidity;
+    wind_speed = data.wind.speed;
+    description = data.weather[0].description;
 
-    // update HTML code with weather data
+    const finalWindSpeed = parseInt(wind_speed) * 3.6;
+
     document.getElementById("temperature").innerHTML = temperature.toFixed(1) + '°C';
     document.getElementById("feels-like").innerHTML = 'Feels Like: ' + feels_like.toFixed(1) + '°C';
     document.getElementById("humidity").innerHTML = 'Humidity: ' + humidity + '%';
-    document.getElementById("wind-speed").innerHTML = 'Wind Speed: ' + wind_speed*3.6.toFixed(1) + 'km/h';
+    document.getElementById("wind-speed").innerHTML = 'Wind Speed: ' + finalWindSpeed.toFixed(1) + 'km/h';
     document.getElementById("description").innerHTML = description;
   })
   .catch(error => console.log(error));
